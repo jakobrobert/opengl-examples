a97f4e3dae1f811b16ce76bc1279971b3cc07c71
Jack-42 <jakob-robert@hotmail.de>
2020-11-18 00:30:19 +0100

    Change OpenGL version to 2.0 to make it work on current test system

--- examples/01-window/src/main.cpp
+++ examples/01-window/src/main.cpp
@@ -18,10 +18,10 @@ int main()
         std::cerr << "Failed to initialize GLFW!" << std::endl;
     }
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
     GLFWwindow* window = glfwCreateWindow(1280, 720, "01 - Window", nullptr, nullptr);
     if (!window) {
