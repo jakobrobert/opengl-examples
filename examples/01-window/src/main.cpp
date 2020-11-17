602adeeef5ee63ee56dd2c48c043c3547a6c947e
Jack-42 <jakob-robert@hotmail.de>
2020-11-18 00:21:48 +0100

    WIP: Create glfw window (but linking to glfw fails)

--- examples/01-window/src/main.cpp
+++ examples/01-window/src/main.cpp
@@ -1,12 +1,51 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <iostream>
+
 #include "core/Core.hpp"
+
+static void errorCallback(int error, const char* description)
+{
+    std::cout << "GLFW error " << error << ": " << description << std::endl;
+}
 
 int main()
 {
-    Core core;
-    core.init();
+    glfwSetErrorCallback(errorCallback);
+    
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW!" << std::endl;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "01 - Window", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window!" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGL()) {
+        std::cerr << "Failed to create GLFW window!" << std::endl;
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        return 1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 
     return 0;
 }
