86a8a2a1528a4796416ea7c7fbc79cacd863e9b7
Jack-42 <jakob-robert@hotmail.de>
2020-11-21 23:21:16 +0100

    Extract function createWindow()

--- examples/02-triangle/src/main.cpp
+++ examples/02-triangle/src/main.cpp
@@ -5,37 +5,13 @@
 
 #include "core/Core.hpp"
 
-static void errorCallback(int error, const char* description)
-{
-    std::cout << "GLFW error " << error << ": " << description << std::endl;
-}
+static GLFWwindow* createWindow();
+static void errorCallback(int error, const char* description);
 
 int main()
 {
-    glfwSetErrorCallback(errorCallback);
-    
-    if (!glfwInit()) {
-        std::cerr << "Failed to initialize GLFW!" << std::endl;
-    }
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "02 - Triangle", nullptr, nullptr);
+    GLFWwindow* window = createWindow();
     if (!window) {
-        std::cerr << "Failed to create GLFW window!" << std::endl;
-        glfwTerminate();
-        return 1;
-    }
-
-    glfwMakeContextCurrent(window);
-
-    if (!gladLoadGL()) {
-        std::cerr << "Failed to create GLFW window!" << std::endl;
-        glfwDestroyWindow(window);
-        glfwTerminate();
         return 1;
     }
 
@@ -52,4 +28,42 @@ int main()
     glfwTerminate();
 
     return 0;
+}
+
+static GLFWwindow* createWindow()
+{
+    glfwSetErrorCallback(errorCallback);
+    
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW!" << std::endl;
+        return nullptr;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "02 - Triangle", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window!" << std::endl;
+        glfwTerminate();
+        return nullptr;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGL()) {
+        std::cerr << "Failed to initialize glad!" << std::endl;
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        return nullptr;
+    }
+
+    return window;
+}
+
+static void errorCallback(int error, const char* description)
+{
+    std::cout << "GLFW error " << error << ": " << description << std::endl;
 }
