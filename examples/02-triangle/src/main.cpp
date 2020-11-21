25d6dc55bd0b69f8f4bc8c2ed847dc7e647eac41
Jack-42 <jakob-robert@hotmail.de>
2020-11-22 00:42:06 +0100

    WIP: Create shader (failing to compile, so error handling works properly)

--- examples/02-triangle/src/main.cpp
+++ examples/02-triangle/src/main.cpp
@@ -2,11 +2,14 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <string>
+#include <vector>
 
 #include "core/Core.hpp"
 
 static GLFWwindow* createWindow();
 static void errorCallback(int error, const char* description);
+static GLuint createShader(const std::string& vertexSource, const std::string& fragmentSource);
 
 int main()
 {
@@ -39,6 +42,12 @@ int main()
     // define vertex layout
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+
+    // create shader
+    GLuint shader = createShader("foo", "bar");
+    if (!shader) {
+        return 1;
+    }
 
     glClearColor(0.0f, 0.0, 0.0f, 1.0f);
 
@@ -94,4 +103,92 @@ static GLFWwindow* createWindow()
 static void errorCallback(int error, const char* description)
 {
     std::cout << "GLFW error " << error << ": " << description << std::endl;
+}
+
+static GLuint createShader(const std::string& vertexSource, const std::string& fragmentSource)
+{
+    // compile vertex shader
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    const GLchar* vertexSourceCStr = vertexSource.c_str();
+    glShaderSource(vertexShader, 1, &vertexSourceCStr, 0);
+    glCompileShader(vertexShader);
+
+    // check for compile errors
+    GLint isCompiled = 0;
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &isCompiled);
+    if (!isCompiled) {
+        // get error message
+        GLint maxLength = 0;
+        glGetShaderiv(vertexShader, GL_INFO_LOG_LENGTH, &maxLength);
+        std::vector<GLchar> infoLog(maxLength);
+        glGetShaderInfoLog(vertexShader, maxLength, &maxLength, &infoLog[0]);
+
+        // clean up
+        glDeleteShader(vertexShader);
+
+        std::cerr << "Failed to compile vertex shader!" << std::endl;
+        std::cerr << infoLog.data() << std::endl;
+        
+        return 0;
+    }
+
+    // compile fragment shader
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    const GLchar* fragmentSourceCStr = fragmentSource.c_str();
+    glShaderSource(fragmentShader, 1, &fragmentSourceCStr, 0);
+    glCompileShader(fragmentShader);
+
+    // check for compile errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &isCompiled);
+    if (isCompiled == GL_FALSE)
+    {
+        // get error message
+        GLint maxLength = 0;
+        glGetShaderiv(fragmentShader, GL_INFO_LOG_LENGTH, &maxLength);
+        std::vector<GLchar> infoLog(maxLength);
+        glGetShaderInfoLog(fragmentShader, maxLength, &maxLength, &infoLog[0]);
+        
+        // clean up
+        glDeleteShader(fragmentShader);
+        glDeleteShader(vertexShader);
+
+        std::cerr << "Failed to compile fragment shader!" << std::endl;
+        std::cerr << infoLog.data() << std::endl;
+
+        return 0;
+    }
+
+    // link vertex and fragment shader into program
+    GLuint program = glCreateProgram();
+    glAttachShader(program, vertexShader);
+    glAttachShader(program, fragmentShader);
+    glLinkProgram(program);
+
+    // check for link errors
+    GLint isLinked = 0;
+    glGetProgramiv(program, GL_LINK_STATUS, (int *)&isLinked);
+    if (isLinked == GL_FALSE)
+    {
+        // get error message
+        GLint maxLength = 0;
+        glGetProgramiv(program, GL_INFO_LOG_LENGTH, &maxLength);
+        std::vector<GLchar> infoLog(maxLength);
+        glGetProgramInfoLog(program, maxLength, &maxLength, &infoLog[0]);
+        
+        // clean up
+        glDeleteProgram(program);
+        glDeleteShader(vertexShader);
+        glDeleteShader(fragmentShader);
+
+        std::cerr << "Failed to link program!" << std::endl;
+        std::cerr << infoLog.data() << std::endl;
+
+        return 0;
+    }
+
+    // clean up, do not need temporary compile results
+    glDetachShader(program, vertexShader);
+    glDetachShader(program, fragmentShader);
+
+    return program;
 }
