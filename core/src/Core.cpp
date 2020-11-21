97afe6851deef2aefd7ddfdc79f7e294a74a4c1e
Jack-42 <jakob-robert@hotmail.de>
2020-11-21 01:42:00 +0100

    Setup project
    * Add .gitignore
    
    * Setup basic CMake, add build and clean scripts
    
    * Add hello world program
    
    * Add glfw as git submodule
    
    * WIP: Link glfw library (fails)
    
    * Disable glfw build of examples, tests, docs, install
    
    * WIP: Add dummy source file to core to make it compile, but now linker error
    
    * Fix linker error by separating header and source, can only link source
    
    * Add core to include directories, adjust include paths
    
    * Add glad library
    
    * Fix include directories
    
    * WIP: Create glfw window (but linking to glfw fails)
    
    * Fix linking to glfw
    
    * Change OpenGL version to 2.0 to make it work on current test system
    
    * Test opengl with glClear call, works fine
    
    * Change OpenGL version to 3.3

---
+++ core/src/Core.cpp
@@ -0,0 +1,8 @@
+#include "core/Core.hpp"
+
+#include <iostream>
+
+void Core::init()
+{
+    std::cout << "Init Core" << std::endl;
+}
---
+++ examples/01-window/src/main.cpp
@@ -0,0 +1,55 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+#include "core/Core.hpp"
+
+static void errorCallback(int error, const char* description)
+{
+    std::cout << "GLFW error " << error << ": " << description << std::endl;
+}
+
+int main()
+{
+    glfwSetErrorCallback(errorCallback);
+    
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW!" << std::endl;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "01 - Window", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window!" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGL()) {
+        std::cerr << "Failed to create GLFW window!" << std::endl;
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        return 1;
+    }
+
+    glClearColor(1.0f, 0.0, 1.0f, 1.0f);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}
