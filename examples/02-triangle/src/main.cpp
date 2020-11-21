81b2a1a02f66df7d3a94e3cec076c1506a3f349d
Jack-42 <jakob-robert@hotmail.de>
2020-11-21 23:59:24 +0100

    WIP: Create vertex array, bufer, draw triangle (nothing visible, need shader)

--- examples/02-triangle/src/main.cpp
+++ examples/02-triangle/src/main.cpp
@@ -15,10 +15,38 @@ int main()
         return 1;
     }
 
+    // create vertex array
+    GLuint vertexArray;
+    glGenVertexArrays(1, &vertexArray);
+    glBindVertexArray(vertexArray);
+
+    // create vertex buffer, bind it to the vertex array and copy data into it
+    GLuint vertexBuffer;
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+
+    // define vertex data and upload it to buffer
+    // TODO: change to 2D-coord
+    float vertices[3 * 3] = {
+        // in xy-plane, counter-clockwise order
+        -0.5f, -0.5f, 0.0f, // left bottom
+        0.5f, -0.5f, 0.0f,  // right bottom
+        0.0f, 0.5, 0.0f,    // center top
+    };
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // define vertex layout
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+
     glClearColor(0.0f, 0.0, 0.0f, 1.0f);
 
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
+
+        // draw the triangle
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
