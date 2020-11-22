21cca3483019e0de96a8ef092ed703db6265d571
Jack-42 <jakob-robert@hotmail.de>
2020-11-22 01:52:45 +0100

    Change window size

--- examples/02-triangle/src/main.cpp
+++ examples/02-triangle/src/main.cpp
@@ -113,7 +113,7 @@ static GLFWwindow* createWindow()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "02 - Triangle", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(800, 800, "02 - Triangle", nullptr, nullptr);
     if (!window) {
         std::cerr << "Failed to create GLFW window!" << std::endl;
         glfwTerminate();
