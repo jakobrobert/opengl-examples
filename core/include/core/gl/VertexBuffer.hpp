5fc5f39f65de467599a216475698af658ec90837
Jack-42 <jakob-robert@hotmail.de>
2020-12-01 18:44:19 +0100

    Add class IndexBuffer

--- core/include/core/gl/VertexBuffer.hpp
+++ core/include/core/gl/VertexBuffer.hpp
@@ -5,6 +5,7 @@
 class VertexBuffer : public OpenGLObject
 {
 public:
+    // TODO: change to count -> type is known, size in bytes does not make sense
     VertexBuffer(const float* data, unsigned int size);
     virtual ~VertexBuffer();
 
