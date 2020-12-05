c94ef8830a366ff0b93d244c3280059921818e6b
Jack-42 <jakob-robert@hotmail.de>
2020-12-05 20:37:18 +0100

    Begin new project, as copy of Rectangle

---
+++ examples/04-rectangle-uniform-color/src/RectangleRenderer.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "core/Renderer.hpp"
+#include "core/gl/ShaderProgram.hpp"
+#include "core/gl/VertexArray.hpp"
+#include "core/gl/VertexBuffer.hpp"
+#include "core/gl/IndexBuffer.hpp"
+
+class RectangleRenderer : public Renderer
+{
+public:
+    virtual bool onInit() override;
+    virtual void onDestroy() override;
+    virtual void onDraw() override;
+private:
+    ShaderProgram* m_shader = nullptr;
+    VertexArray* m_vertexArray = nullptr;
+    VertexBuffer* m_vertexBuffer = nullptr;
+    IndexBuffer* m_indexBuffer = nullptr;
+};
