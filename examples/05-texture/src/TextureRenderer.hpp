e38427a84476f45b14307353f5f6054de1dd12e5
Jack-42 <jakob-robert@hotmail.de>
2020-12-15 16:26:07 +0100

    Add texture example (#18)
    * Create new example based on Rectangle (with vertex color)
    
    * Change vertex color from white to yellow
    
    * Use prefix for variables in shader
    
    * Add wall texture asset
    
    * Add stb_image library to core
    
    * Simplify CMake: publicly propagate glad include dir
    
    * Begin Texture class, fix linking of stb_image
    
    * Implement Texture class
    
    * Integrate texture into renderer
    
    define texture coords, use texture in shader, multiply texture and vertex color
    
    Close #13

---
+++ examples/05-texture/src/TextureRenderer.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "core/Renderer.hpp"
+#include "core/gl/ShaderProgram.hpp"
+#include "core/gl/VertexArray.hpp"
+#include "core/gl/VertexBuffer.hpp"
+#include "core/gl/IndexBuffer.hpp"
+#include "core/gl/Texture.hpp"
+
+class TextureRenderer : public Renderer
+{
+public:
+    virtual bool onInit() override;
+    virtual void onDestroy() override;
+    virtual void onDraw() override;
+private:
+    ShaderProgram* m_shader = nullptr;
+    VertexArray* m_vertexArray = nullptr;
+    VertexBuffer* m_vertexBuffer = nullptr;
+    IndexBuffer* m_indexBuffer = nullptr;
+    Texture* m_texture = nullptr;
+
+    int m_textureUniformLocation = -1;
+};
