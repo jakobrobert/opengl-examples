9aa72f0dd985823bed0b06826d2fb56881455382
Jack-42 <jakob-robert@hotmail.de>
2020-12-19 23:21:04 +0100

    Add glm library to core And add missing files from previous commit

---
+++ examples/06-transformations/src/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <core/Window.hpp>
+
+#include "TransformationsRenderer.hpp"
+
+int main()
+{
+    try
+    {
+        TransformationsRenderer renderer;
+        Window window("06 - Transformations", 800, 800, &renderer);
+        window.runEventLoop();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
