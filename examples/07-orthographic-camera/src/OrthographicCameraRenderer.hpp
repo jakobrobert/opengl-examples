95e967f115d61783ea2f63a8e5ea1f17e3b02267
Jack-42 <jakob-robert@hotmail.de>
2021-01-26 14:41:12 +0100

    Begin new example as copy of old one

---
+++ examples/07-orthographic-camera/src/OrthographicCameraRenderer.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <core/Renderer.hpp>
+#include <core/gl/ShaderProgram.hpp>
+#include <core/gl/VertexArray.hpp>
+#include <core/gl/VertexBuffer.hpp>
+#include <core/gl/IndexBuffer.hpp>
+#include <core/gl/Texture.hpp>
+#include <core/Transform2D.hpp>
+
+class OrthographicCameraRenderer : public Renderer
+{
+public:
+    virtual bool onInit() override;
+    virtual void onDestroy() override;
+    virtual void onUpdate(float time) override;
+    virtual void onDraw() override;
+private:
+    ShaderProgram* m_shader = nullptr;
+    VertexArray* m_vertexArray = nullptr;
+    VertexBuffer* m_vertexBuffer = nullptr;
+    IndexBuffer* m_indexBuffer = nullptr;
+    Texture* m_texture = nullptr;
+    Transform2D m_transform;
+
+    int m_modelMatrixUniformLocation = -1;
+    int m_textureUniformLocation = -1;
+};
