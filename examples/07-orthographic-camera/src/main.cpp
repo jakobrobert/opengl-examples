456f7fb927e97e82b60942275fe644a79673ea65
Jack-42 <jakob-robert@hotmail.de>
2021-01-26 16:40:10 +0100

    Change initial window size, must not be square anymore

--- examples/07-orthographic-camera/src/main.cpp
+++ examples/07-orthographic-camera/src/main.cpp
@@ -11,7 +11,7 @@ int main()
     try
     {
         OrthographicCameraRenderer renderer;
-        Window window("07 - Orthographic Camera", 800, 800, &renderer);
+        Window window("07 - Orthographic Camera", 800, 600, &renderer);
         window.runRenderLoop();
     } catch (const std::exception& e) {
         std::cerr << e.what() << std::endl;
