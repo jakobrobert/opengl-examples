6cb2243bd527bbe846abb851d02acc49cd7d6673
Jack-42 <jakob-robert@hotmail.de>
2021-02-17 14:17:02 +0100

    Clean up includes

--- examples/07-orthographic-camera/src/OrthographicCameraRenderer.cpp
+++ examples/07-orthographic-camera/src/OrthographicCameraRenderer.cpp
@@ -2,12 +2,10 @@
 
 #include <glad/glad.h>
 #include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
 #include <iostream>
 #include <string>
-#include <vector>
 #include <cmath>
 
 #include <core/Window.hpp>
